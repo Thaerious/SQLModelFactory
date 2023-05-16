b6696f10c722a3b25f064572f38e2c9672a16e64
Ed Armstrong <ed@frar.ca>
2023-05-16 10:12:59 -0400

    checking

--- src/Model.js
+++ src/Model.js
@@ -1,5 +1,31 @@
 import expandModels from "./expandModels.js";
 
+class ValueProxy {
+    constructor(value, root) {
+        this.value = value;
+    }
+
+    get(target, prop) {
+        if (prop === "$deRef") return this.$deRef(target);
+        if (!this.model[prop]) return Reflect.get(...arguments);
+
+        if (!value && this[prop]) {
+            return this[prop](primitive, prop);
+        } else {
+            return typeof value === 'function' ? value.bind(primitive) : value;
+        }
+
+        return new Proxy(this.model[prop], new ModelProxy(this.model[prop], this.root));
+    }
+
+    $deRef(name) {        
+        if (Array.isArray(name)) name = name[0];
+        if (name.startsWith("@")) name = name.substring(1);
+        if (name.startsWith("[]")) name = name.substring(2);
+        return new Proxy(this.model[name], new ModelProxy(this.model[name], this.root));
+    }    
+}
+
 class ModelProxy {
     constructor(model, root) {
         this.root = root || model;
@@ -9,14 +35,11 @@ class ModelProxy {
     get(target, prop) {
         if (prop === "$deRef") return this.$deRef(target);
         if (!this.model[prop]) return Reflect.get(...arguments);
-        return new Proxy(this.model[prop], new ModelProxy(this.model[prop], this.root));
-    }
 
-    $deRef(name) {        
-        if (Array.isArray(name)) name = name[0];
-        if (name.startsWith("@")) name = name.substring(1);
-        if (name.startsWith("[]")) name = name.substring(2);
-        return new Proxy(this.model[name], new ModelProxy(this.model[name], this.root));
+        if (typeof this.model[prop] === "string") {
+            return new ValueProxy(this.model[prop], this.root);
+        }
+        return new Proxy(this.model[prop], new ModelProxy(this.model[prop], this.root));
     }
 }
 
